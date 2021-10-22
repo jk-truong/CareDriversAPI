5550525799e8528cd01746a9f303193f3b66ffe5
jk-truong <41808114+jk-truong@users.noreply.github.com>
2021-10-21 20:21:20 -0700

    Wrote my first unit test on Android

--- app/src/androidTest/java/com/example/caredriverscodingchallenge/ParseJsonStuffTest.kt
+++ app/src/androidTest/java/com/example/caredriverscodingchallenge/ParseJsonStuffTest.kt
@@ -1,25 +1,48 @@
 package com.example.caredriverscodingchallenge
 
 import android.content.Context
+import android.util.Log
 import androidx.test.core.app.ApplicationProvider
 import junit.framework.TestCase
 import org.junit.Before
 import org.junit.Test
 
 class ParseJsonStuffTest : TestCase() {
-    private lateinit var parseJsonStuff: ParseJsonStuff
-    private lateinit var rideItem: List<Ride>
-    // androidx.test.core.app.ApplicationProvider.getApplicationContext()
-
-    @Before
-    fun setup() {
-        parseJsonStuff = ParseJsonStuff()
-    }
+    private var parseJsonStuff: ParseJsonStuff = ParseJsonStuff()
+    private var rideItem: List<Ride> = mutableListOf(
+        // Dummy data
+        Ride(
+            1,
+            false,
+            "2021-06-17T11:18:00Z",
+            "2021-06-17T12:37:00Z",
+            6071,
+            50,
+            18.48,
+            mutableListOf(
+                OrderedWaypoint(
+                    222,
+                    true,
+                    mutableListOf(
+                        Passenger(123, true, "Mark"),
+                        Passenger(121, false, "John")
+                    ),
+                    Location(
+                        "2565 E Underhill Ave, Anaheim 92806",
+                        34.17006916353578,
+                        -118.43274040504944
+                    )
+                )
+            )
+        )
+    )
 
     @Test
     fun testGetEstimatedEarnings_returnsString() {
-        val context = ApplicationProvider.getApplicationContext<Context>()
-        //val result = parseJsonStuff.getEstimatedEarnings()
+        rideItem[0].estimatedEarningsCents = 1000
+        val result = parseJsonStuff.getEstimatedEarnings(rideItem)
+
+        assertEquals("$10.00", result)
     }
 
     @Test
@@ -36,4 +59,5 @@ class ParseJsonStuffTest : TestCase() {
 
     @Test
     fun testGetFormattedDate() {}
+
 }
